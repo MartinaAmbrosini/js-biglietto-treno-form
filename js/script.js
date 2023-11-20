f5a07d30f6bf9b67dc53307a3a4f848bd2eec1cc
Martina Ambrosini <martinaambrosini17@gmail.com>
2023-11-20 13:14:42 +0100

    primo push per impostazione elementi

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Descrizione:**
+// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere
+// Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
+// La risposta finale (o output) sarà anch’essa da scrivere in console.
+// MILESTONE 2: Solo una volta che il milestone 1 sarà completo e funzionante allora finalizzeremo il layout di pagina, in cui l’utente vedrà il form stilizzato e tutto l’output stampato in pagina (il prezzo dovrà sempre essere formattato con massimo due decimali).
